ab1c8b7917a383f75ac1be3e447df1bb1732192c
agjohngabriel <agjohngabriel@gmail.com>
2022-09-24 01:00:15 +0100

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -12,6 +12,7 @@ export default function handler(req, res) {
     //const user = findbyemail()
     //user.wallet.balance == 
     console.log(ob)
+    console.log(req.body)
     return res.status(200).json(ob)
   }
   if(req.body.event === "paymentrequest.pending"){
